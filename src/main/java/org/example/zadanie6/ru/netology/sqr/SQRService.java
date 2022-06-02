d9f81870e83efd774bdd05d587373f53047f1385
Анастасия Пакаева <staroxy63@gmail.com>
2022-06-02 17:02:28 +0300

    contribute edits

--- src/main/java/org/example/zadanie6/ru/netology/sqr/SQRService.java
+++ src/main/java/org/example/zadanie6/ru/netology/sqr/SQRService.java
@@ -1,13 +1,13 @@
 package org.example.zadanie6.ru.netology.sqr;
 
 public class SQRService {
-    public int calcSQRService(int a, int b) {
-        int z = 0;
-        for (int i = 10; i <= 99; i++) {
-            if ((a <= i * i) && (i * i <= b)) {
-                z++;
+    public int calcSQRService(int limit1, int limit2) {
+        int amount = 0;
+        for (int square = 10; square <= 99; square++) {
+            if ((limit1 <= square * square) && (square * square <= limit2)) {
+                amount++;
             }
         }
-        return z;
+        return amount;
     }
 }
--- src/test/java/SQRServiceTest.java
+++ src/test/java/SQRServiceTest.java
@@ -11,4 +11,59 @@ public class SQRServiceTest {
         int actual = service.calcSQRService(200, 300);
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void zeroAllCalcSQRService() {
+        SQRService service = new SQRService();
+
+        int expected = 0;
+        int actual = service.calcSQRService(0, 0);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void zeroLimit1CalcSQRService() {
+        SQRService service = new SQRService();
+
+        int expected = 8;
+        int actual = service.calcSQRService(0, 300);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void zeroLimit2CalcSQRService() {
+        SQRService service = new SQRService();
+
+        int expected = 0;
+        int actual = service.calcSQRService(200, 0);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void MinusCalcSQRService() {
+        SQRService service = new SQRService();
+
+        int expected = 0;
+        int actual = service.calcSQRService(-200, -300);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void IncorrectCalcSQRService() {
+        SQRService service = new SQRService();
+
+        int expected = 0;
+        int actual = service.calcSQRService(300, 200);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void SquareLimitCalcSQRService() {
+        SQRService service = new SQRService();
+
+        int expected = 3;
+        int actual = service.calcSQRService(225, 300);
+        Assertions.assertEquals(expected, actual);
+    }
 }
+
